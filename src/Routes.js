e68d3855735bb5670beb8be75223019d998dcc03
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-03-03 18:11:50 +0500

    initial

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,6 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Header from './components/Header/Header'
-import Footer from './components/footer/Footer'
-import Home from './components/Home/Home'
+
 import React from 'react'
 import GetQuoteForm from './components/GetQuote/GetQuoteForm'
 import Checkout from './components/Cart/Checkout'
--- src/components/Product_Listing/product_listing.js
+++ src/components/Product_Listing/product_listing.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import Header from '../Header/Header'
 import Home from '../Home/Home'
-import Footer from '../footer/Footer'
-import './product_listing.css'
-// import './Product_Listing.scss'
+import Footer from '../Footer/Footer'
+
+import './Product_Listing.scss'
 function Product_Listing() {
   return (
     <div className='container'>
