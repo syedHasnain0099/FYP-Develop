3e07657c3c7ba6e7a243c03dd1c18bacddc1ef10
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-03-16 22:49:41 +0500

    check on password applied

--- src/components/SignUp/validateInfo.js
+++ src/components/SignUp/validateInfo.js
@@ -1,12 +1,15 @@
 export default function validateInfo(values) {
   let errors = {}
   var usernameRegex = /^(?=[a-zA-Z0-9]{6,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/
-  var newRegex= /^(?=.*?[a-z0-9]{6,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/
-  // /(?=.*?[0-9])(?=.*?[A-Za-z]).+/
-  //username is 6-15 characters long, no _ or . at the beginning, no __ or _. or ._ or .. inside, alphanumeric characters allowed, no _ or . at the end
+  var newRegex = /^(?=.*?[a-z0-9]{6,15}$)(?!.*[_.]{2})[^_.].*[^_.]$/
+  var passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*d)(?=.*[@$!%*?&])[A-Za-zd@$!%*?&]{8,}$/
+
+  //  username is 6-15 characters long, no _ or . at the beginning, no __ or _. or ._ or .. inside, alphanumeric characters allowed, no _ or . at the end
   const isValid = newRegex.test(values.username)
+  const passwordValid = passwordRegex.test(values.password)
   // = '/^[a-zA-Z]+$/';
-  var hasSpace = '/^\S+$/'
+  var hasSpace = '/^S+$/'
   // var hasNumbersAtStart = '/^[0-9]/'
   // var isValid = /[a-zA-Z]/.test(values.username) && /^[0-9]$/.test(values.username);
   if (!values.username.trim()) {
@@ -15,8 +18,9 @@ export default function validateInfo(values) {
   // else if (values.username.length < 6 || values.username.length>15) {
   //   errors.username = 'Username needs to be in between 6 and 15 characters long'
   // }
-  else if (!isValid){
-    errors.username = 'Username needs to be in between 6 and 15 characters long\n Username should contain atleast one character,\n Only characters and digits allowed with no space between!'
+  else if (!isValid) {
+    errors.username =
+      'Username needs to be in between 6 and 15 characters long\n Username should contain atleast one character,\n Only characters and digits allowed with no space between!'
   }
   if (!values.email) {
     errors.email = 'Email required'
@@ -25,8 +29,9 @@ export default function validateInfo(values) {
   }
   if (!values.password) {
     errors.password = 'Password is required'
-  } else if (values.password.length < 6) {
-    errors.password = 'Password needs to be 6 characters or more'
+  } else if (!passwordValid) {
+    errors.password =
+      'Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character required'
   }
 
   if (!values.password2) {
