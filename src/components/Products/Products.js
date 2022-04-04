6c42c4f79d60de072ff78d680adba75fdd79d9ac
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-04-05 00:43:46 +0500

    returning product id

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -77,7 +77,7 @@ function Products() {
                     <p class='card-text'>
                       Rs {product.price} / {product.duration}
                     </p>
-                    <Link to='/'>
+                    <Link to={`/product/${product.id}`}>
                       <button className='btn btn-outline-dark mt-2 mb-2 mr-2'>
                         View Product
                       </button>
