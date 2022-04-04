a397f00bd5ef7c01e5e83d99c11aa903fbdac187
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-04-05 00:41:03 +0500

    getrequested ads Api is not giving response or data

--- src/admin/ApproveAd.js
+++ src/admin/ApproveAd.js
@@ -1,31 +1,21 @@
 import productService from '../services/ProductService'
-import React from 'react'
+import React, { useEffect } from 'react'
 
-const subcategoryName = ''
-const categoryId = 1
-const searchKeyword = 'i phone'
-
-const getRequestedAds = () => {
-  productService
-    .getRequestedAds()
-    .then((response) => {
-      console.log(response)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
-const searchByKeyword = (keyword, subcategoryName) => {
-  productService
-    .search(keyword, '')
-    .then((res) => {
-      console.log('search results: ', res)
-    })
-    .catch((err) => console.log(err))
-}
 function ApproveAd() {
-  getRequestedAds()
-  searchByKeyword(searchKeyword, 'Mobiles')
+  const getRequestedAds = () => {
+    productService
+      .getRequestedAds()
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    getRequestedAds()
+  }, [])
   return <div>ApproveAd</div>
 }
 
