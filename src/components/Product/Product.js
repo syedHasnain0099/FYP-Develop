9a21f44493b16b69b6a2da1ed0b1d7c41c6d15ec
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-04-05 23:32:00 +0500

    initial

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,59 +1,98 @@
 import React, { useEffect, useState } from 'react'
 import productService from '../../services/ProductService'
-import { Link, useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
+import { Skeleton } from '@mui/material'
+import { Link } from 'react-router-dom'
 function Product(props) {
   const [product, setProduct] = useState({})
   const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
   let { productId } = useParams()
   const loadSingleProduct = (productId) => {
-    console.log("come on")
+    console.log('come on')
+    setLoading(true)
     productService
       .findOne(productId)
       .then((response) => {
-        console.log("product: ",response)
+        console.log('product: ', response)
         setProduct(response)
+        setLoading(false)
       })
       .catch((err) => {
-        console.log("inside catch")
+        console.log('inside catch')
         console.log(err)
       })
-    //api to fetch data of product
-    //set response equals to setProduct(response)
+  }
+
+  const Loading = () => {
+    return (
+      <>
+        <div className='col-md-3'>
+          <Skeleton height={350} />
+        </div>
+        <div className='col-md-3'>
+          <Skeleton height={350} />
+        </div>
+        <div className='col-md-3'>
+          <Skeleton height={350} />
+        </div>
+        <div className='col-md-3'>
+          <Skeleton height={350} />
+        </div>
+      </>
+    )
+  }
+  const ShowProducts = () => {
+    return (
+      <div className='col-4 mb-3'>
+        <div class='card h-100 text-center p-4' key={product.id}>
+          <img
+            class='card-img-top'
+            src={product.image_urls[0]}
+            alt={product.name}
+            height='250px'
+            //style={{ maxHeight: '100%', maxWidth: '100%' }}
+          />
+          <div class='card-body'>
+            <h5
+              class='card-title mb-1
+                     lead fw-bold'
+            >
+              {product.name}
+            </h5>
+            <p class='card-text'>{product.description}...</p>
+            <p class='card-text'>
+              Rs {product.price} / {product.duration}
+            </p>
+            <Link to={`/product/${product.id}`}>
+              <button className='btn btn-outline-dark mt-2 mb-2 mr-2'>
+                View Product
+              </button>
+            </Link>
+            <Link to='/'>
+              <button className='btn btn-outline-dark mt-2 mb-2 mr-2'>
+                Add to cart
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
   }
   useEffect(() => {
-    console.log("here",productId)
     loadSingleProduct(productId)
   }, [])
   return (
-    <div className='container my-5 py-5'>
-      <div className='row'>
-        <div className='col-12 mb-5'>
-          <h1 className='display-6 fw-bolder text-center'>
-            Product Description
-          </h1>
-          <hr />
+    <div>
+      <div className='container my-5 py-5'>
+        <div className='row'>
+          <div className='col-12 mb-5'>
+            <h1 className='display-6 fw-bolder text-center'>Products</h1>
+            <hr />
+          </div>
         </div>
-      </div>
-      <div class='card h-100 text-center p-4' key={product.id}>
-        <img
-          class='card-img-top'
-          src={product.image_urls[0]}
-          alt={product.name}
-          height='250px'
-          //style={{ maxHeight: '100%', maxWidth: '100%' }}
-        />
-        <div class='card-body'>
-          <h5 class='card-title mb-1 lead fw-bold'>{product.name}</h5>
-          <p class='card-text'>{product.description}</p>
-          <p class='card-text'>
-            Rs {product.price} / {product.duration}
-          </p>
-
-          <Link to='/'>
-            <button className='btn btn-outline-dark mt-2 mb-2 mr-2'>
-              Add to cart
-            </button>
-          </Link>
+        <div className='row justify-content-center'>
+          {loading ? Loading() : ShowProducts()}
         </div>
       </div>
     </div>
