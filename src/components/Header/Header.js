b938c40f1bed31b7e2c2f3290bb3b278ea109769
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-04-26 02:19:07 +0500

    updated header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -123,9 +123,9 @@ function Header({ history }) {
             </NavLink>
           )}
 
-          <NavLink to='/cart' className='btn btn-outline-dark ms-2'>
+          {/* <NavLink to='/cart' className='btn btn-outline-dark ms-2'>
             <i className='fa fa-shopping-cart me-1'></i> Cart(0)
-          </NavLink>
+          </NavLink> */}
         </div>
       </div>
     </nav>
