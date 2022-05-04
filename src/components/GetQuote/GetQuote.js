f663c323cea0e2e4145aab89dc905b842458717e
syedHasnain0099 <syedhasnain0099@gmail.com>
2022-05-05 01:57:10 +0500

    changes made at front end getQuote

--- src/components/GetQuote/GetQuote.js
+++ src/components/GetQuote/GetQuote.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { Skeleton } from '@mui/material'
 import productService from '../../services/ProductService'
 import quoteService from '../../services/QuoteService'
@@ -98,6 +98,68 @@ function GetQuote() {
       </>
     )
   }
+  const ShowProducts1 = () => {
+    console.log('running')
+
+    return (
+      <>
+        {data.map((product) => {
+          return (
+            <>
+              <div className='' style={{ marginTop: '30px', padding: '20px' }}>
+                <div className='card-card'>
+                  <div className='card-body'>
+                    <div className='card-img-actions'>
+                      <img
+                        src={product.image_urls[0]}
+                        className='card-img-top'
+                        // width='96'
+                        // height='350'
+                        // alt=''
+                        height='250px'
+                      />
+                    </div>
+                  </div>
+                  <div className='card-body bg-light text-center'>
+                    <div className='mb-2'>
+                      <h5 className='font-weight-bold mb-2'>
+                        <Link
+                          to={`/products/${product.id}`}
+                          className='text-default mb-2'
+                          data-abc='true'
+                        >
+                          {product.name}
+                        </Link>
+                      </h5>
+                      <p class='text-muted'>
+                        {product.description.substring(0, 20)}...
+                      </p>
+                    </div>
+                    <h3 className='mb-0 font-weight-semibold'>
+                      Rs {product.rent} / day
+                    </h3>
+
+                    {product.reviews.length > 0 &&
+                      Array(product.reviews[0].rating)
+                        .fill()
+                        .map((_, i) => (
+                          <span style={{ color: '#ffd700' }}>&#9733;</span>
+                        ))}
+                    <div className='text-muted mb-3'>
+                      {product.reviews.length} reviews
+                    </div>
+                    <Link to={`/products/${product.id}`}>
+                      <h6>Details</h6>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </>
+          )
+        })}
+      </>
+    )
+  }
   const ShowProducts = () => {
     return (
       <>
@@ -136,7 +198,7 @@ function GetQuote() {
     <div className='getquote-form-container'>
       <span className='getquote-close-btn'>×</span>
       <div className='getquote-form-content-left'>
-        {loading ? <Loading /> : <ShowProducts />}
+        {loading ? <Loading /> : <ShowProducts1 />}
       </div>
       <div className='getquote-form-content-right'>
         <form className='getquote-form' onSubmit={submitHandler}>
@@ -198,6 +260,8 @@ function GetQuote() {
               name='quantity'
               placeholder='Quantity'
               onChange={quantityChangeHandler}
+              min='1'
+              max='10'
             />
           </div>
 
