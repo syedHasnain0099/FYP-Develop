7dca69243eec6a796b955d5f079df6cd09339948
Maryam-Inam <maryaminam00@gmail.com>
2022-05-28 17:05:21 +0500

    added redirection to checkout page

--- src/components/StripeCheckoutButton/StripeCheckoutButton.js
+++ src/components/StripeCheckoutButton/StripeCheckoutButton.js
@@ -1,21 +1,100 @@
-import React, { useEffect, useState } from 'react'
-import StripeCheckout from 'react-stripe-checkout'
+import React, { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
 // import axios from 'axios'
 // import { useDispatch, useSelector } from 'react-redux'
 // import { orderCreate } from '../action/orderAction'
-import { useHistory } from 'react-router-dom'
+import { loadStripe } from "@stripe/stripe-js";
+import { userData } from "../../auth";
+import productService from "../../services/ProductService";
+import orderService from "../../services/OrderService";
+import { useHistory } from "react-router-dom";
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100
+  const { id } = userData();
+  const productId = "29";
+  const shippId = 1;
+  const priceForStripe = price * 100;
+  console.log("price: ", priceForStripe);
   const publishablekey =
-    'pk_test_51HHt8xHGRVR8FaKwkV1B1owxunUmN2R6pcG7uCDjT9TjCyBYUQThnznWfJFKh37ZryY3sAkOxS6HUa7kgoCjgzqK00UQw870yQ'
-  const [token, setToken] = useState(null)
+    "pk_test_51L21M9CkQckw00WvUtYFfgcAmm9NQzQ8pI4JlVRLlIoH8jrQV9bFOuN6XBJtNFvaXbMcUueCaU2IotdF1zabgWqy00xR8aRYtZ";
 
+  // const publishablekey =
+  //   "pk_test_51L21M9CkQckw00WvUtYFfgcAmm9NQzQ8pI4JlVRLlIoH8jrQV9bFOuN6XBJtNFvaXbMcUueCaU2IotdF1zabgWqy00xR8aRYtZ";
+  const [productData, setProductData] = useState([]);
+  const [clientSecretKey, setclientSecretKey] = useState("");
+  const stripePromise = loadStripe(publishablekey);
+  const [token, setToken] = useState(null);
+  // const stripe = {};
+
+  const getProductData = () => {
+    productService.findOneProduct(productId).then((data) => {
+      console.log("products inside payment page: ", data);
+      setProductData(data);
+    });
+  };
+  const handleBuy = async () => {
+    const stripe = await stripePromise;
+    console.log("product id: ", productId);
+    console.log("user id: ", id);
+    console.log("total: ", priceForStripe);
+    console.log("shipp id: ", shippId);
+    orderService
+      .postOrder(productId, priceForStripe, id, shippId)
+      .then((data) => {
+        console.log("data:", data.id);
+        stripe.redirectToCheckout({
+          sessionId: data.id,
+        });
+
+        // setclientSecretKey(data.paymentIntent.client_secret);
+        // console.log("client secret id: ", clientSecretKey);
+      }).catch = (err) => {
+      console.log(err);
+    };
+  };
+
+  const collectAccountDetails = async () => {
+    const stripe = await stripePromise;
+
+    stripe
+      .collectBankAccountForPayment({
+        clientSecret: clientSecretKey,
+        params: {
+          payment_method_type: "us_bank_account",
+          payment_method_data: {
+            billing_details: {
+              name: "Jenny Rosen",
+              email: "jenny@example.com",
+            },
+          },
+        },
+      })
+      .then(function (result) {
+        // Handle result.error or result.paymentIntent
+      });
+  };
+  const confirmPayment = async () => {
+    console.log("client secret key: ", clientSecretKey);
+    const stripe = await stripePromise;
+    stripe
+      .confirmCardPayment(clientSecretKey, {
+        return_url: "http://localhost:3000",
+      })
+      .then(function (result) {
+        console.log("payment result: ", result.paymentIntent);
+      });
+  };
+  useEffect(() => {
+    getProductData();
+    handleBuy();
+    // collectAccountDetails();
+    // confirmPayment();
+  }, []);
   const onToken = (token) => {
-    setToken(token)
-    console.log(token)
+    setToken(token);
+    console.log(token);
     // alert("payment success");
-  }
+  };
 
   // const cart = useSelector((state) => state.cart)
   // const { cartItems } = cart
@@ -42,7 +121,7 @@ const StripeCheckoutButton = ({ price }) => {
   //       //alert("payment success");
   //     } catch (error) {
   //       console.log(error)
-  //       // alert("payment fail");
+  //       // alert("payment failed");
   //     }
   //   }
 
@@ -50,16 +129,16 @@ const StripeCheckoutButton = ({ price }) => {
   // }, [token])
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='LBWF Electronic Ltd.'
-      currency='USD'
+      label="Pay Now"
+      name="LBWF Electronic Ltd."
+      currency="USD"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishablekey}
     />
-  )
-}
+  );
+};
 
-export default StripeCheckoutButton
+export default StripeCheckoutButton;
